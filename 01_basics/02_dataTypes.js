23bbaaf73822f965c5d0682c078a57f6285a9890
hiteshkunjir <67042576+hiteshkunjir@users.noreply.github.com>
2023-09-26 04:39:04 +0000

    new tutorials

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,8 @@
+
+let age = 20
+let name = "hitesh"
+let standard = null
+let gender = false
+
+console.log([age, name, standard]);
+console.log(typeof gender);
---
+++ 01_basics/variable.js
@@ -0,0 +1,12 @@
+
+const accountId = 2;
+
+var accountEmail = "hk@hk.com"
+let accountPassword = "1234567890"
+accountCity = "Pune"
+
+// accountId = 3
+
+console.log(accountId)
+console.log([accountId, accountEmail, accountPassword, accountCity])
+
